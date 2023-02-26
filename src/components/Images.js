4d74a7e4567eeb73e680db474efdfaf7278e20bf
sakilshikdar <sakilshikdar2504@gmail.com>
2023-02-26 12:37:56 +0600

    modal-added

--- src/components/Images.js
+++ src/components/Images.js
@@ -10,7 +10,7 @@ const Images = () => {
 
   return (
     <>
-      <h1 className="text-center mt-6 underline text-2xl">Results for {searchImage || ''}</h1>
+      <h1 className="text-center mt-6 underline text-2xl">Results for {searchImage || 'natural'}</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-10 my-10 max-w-7xl mx-auto px-4">
         {isLoading ? "" : response.map((data, key) => <Image key={key} data={data}  setPhotos={setPhotos} />)}
       </div>
---
+++ src/components/Modal.js
@@ -0,0 +1,58 @@
+import { format } from 'date-fns';
+import React from 'react'
+import { AiOutlineLike } from 'react-icons/ai';
+
+const Modal = ({ Photos }) => {
+    const { id, urls, user, created_at, likes, description } = Photos;
+    return (
+        <>
+            <input type="checkbox" id="unsplash-modal" className="modal-toggle modal" />
+            <div className="modal">
+                <div className="w-1/2 h-11/12">
+
+                    <label htmlFor="unsplash-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <img
+                        src={urls.full}
+                        alt={user.username}
+                        classNameName=""
+                    />
+                <div className="p-5 pb-0 flex flex-col md:flex-row items-start md:items-center justify-between">
+                    <article className="flex items-center justify-start">
+                        <img
+                            src={user.profile_image.medium}
+                            alt={user.username}
+                            className="rounded-full mr-2 w-10 md:w-auto delay-1000"
+                        />
+                        <ul>
+                            <li className="text-slate-800 font-bold">{user.name}</li>
+                            <li className="text-sm text-slate-800 opacity-75">
+                                {format(new Date(created_at), "dd MMMM yyyy")}
+                            </li>
+
+                            <p>{description}</p>
+                        </ul>
+                    </article>
+                    <article className="mt-5 md:mt-0">
+                        <div>
+                            <a
+                                href={`https://instagram.com/${user.instagram_username}`}
+                                className="text-sm text-slate-800 opacity-75 underline"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                {user.instagram_username}
+                            </a>
+                        </div>
+                        <small className="flex items-center  text-slate-800 opacity-75">
+                            <AiOutlineLike className="mr-1" size={25} /> {likes}
+                        </small>
+                    </article>
+                </div>
+            </div>
+                </div>
+            
+        </>
+    )
+}
+
+export default Modal
