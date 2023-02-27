e7fbbcc3c510e2ee578a105f4a2fec935b026fe9
sakilshikdar <sakilshikdar2504@gmail.com>
2023-02-27 19:21:10 +0600

    modal-updated

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,8 @@
-import { hover } from '@testing-library/user-event/dist/hover';
 import { format } from 'date-fns';
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { AiOutlineLike } from 'react-icons/ai';
-import share from './share';
+import { HiDownload } from 'react-icons/hi';
+import { IoIosShareAlt } from 'react-icons/io';
 
 const Modal = ({ Photos }) => {
     const { id, urls, user, created_at, likes, description } = Photos;
@@ -13,15 +13,15 @@ const Modal = ({ Photos }) => {
         <>
             <input type="checkbox" id="unsplash-modal" className="modal-toggle modal" />
             <div className="modal opacity-90">
-                <div className="w-1/2 h-11/12">
-                    <label htmlFor="unsplash-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <div className="w-50 h-50">
+                    <label htmlFor="unsplash-modal" className="btn btn-sm btn-circle absolute lg:right-72 lg:top-10">✕</label>
                             <img
                         src={urls.full}
                         alt={user.username}
                         className="modal-image"
                     />
                     <div className="p-5 pb-0 flex flex-col md:flex-row items-start md:items-center justify-between bg-slate-50">
-                        <article className="flex items-center justify-start">
+                        <article className="flex items-center justify-start mb-5 shadow-lg p-4 rounded-md">
                             <img
                                 src={user.profile_image.medium}
                                 alt={user.username}
@@ -37,6 +37,12 @@ const Modal = ({ Photos }) => {
                                 </ul>
                         </article>
                         <article className="mt-5 md:mt-0">
+                            <div className='flex'>
+                            <a href={Photos?.link?.download} className="cursor-pointer"><HiDownload size={30}className=""/></a>
+                            <a href={Photos?.link?.download} className="ml-4 cursor-pointer"><IoIosShareAlt size={30}/></a>
+                            </div>
+                        </article>
+                        <article className="mt-5 md:mt-0">
                             <div>
                                 <a
                                     href={`https://instagram.com/${user.instagram_username}`}
@@ -47,7 +53,7 @@ const Modal = ({ Photos }) => {
                                     {user.instagram_username}
                                 </a>
                             </div>
-                            <small className="flex items-center  text-slate-800 opacity-75">
+                            <small className="flex items-center  text-slate-800 opacity-75 cursor-pointer">
                                 <AiOutlineLike className="mr-1" size={25} /> {likes}
                             </small>
                         </article>
